15095d40655a2c4dcb1d4059fa81f1373b60113a
juancgalueweb <juancgalue@icloud.com>
2022-07-29 00:19:56 -0400

    Dockerizando la app

--- client/src/helpers/axios.js
+++ client/src/helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const baseUrl = process.env.REACT_APP_API_URL;
-const baseUrl = "http://localhost:8080/api";
+const baseUrl = "http://localhost:8000/api";
 
 // console.log("Base URL", baseUrl);
 const KEY = "wines-app";
--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+// Esta ruta funciona
+// const LOCAL_DB_URL = "mongodb://localhost/wine_blog_db";
+
+// Ruta para el contenedor de docker
+const LOCAL_DB_URL = "mongodb://mongo/wine_blog_db";
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(process.env.LOCAL_DB_URL);
+  await mongoose.connect(`${LOCAL_DB_URL}`);
 }
