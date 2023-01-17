e519bb8d86adcffe2af0ce7bc53f5c2057cccee5
juancgalueweb <juancgalue@icloud.com>
2023-01-17 01:47:51 -0300

    Config updated

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,10 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
+mongoose.set('strictQuery', false);
 const DB_URL = process.env.MONGO_URI;
 
 try {
   await mongoose.connect(DB_URL);
-  console.log("Connected to the DB 🔥🔥🔥");
+  console.log('Connected to the DB 🔥🔥🔥');
 } catch (error) {
   handleError(error);
 }
